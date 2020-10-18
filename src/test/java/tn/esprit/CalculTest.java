bbbd97da1d1f8db997027cf8aea3fd4ed05e0408
Resprit30 <rim.ammar@esprit.tn>
2020-10-18 16:58:45 +0100

    My First Commit

---
+++ src/test/java/tn/esprit/CalculTest.java
@@ -0,0 +1,19 @@
+package tn.esprit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculTest {
+	Calcul calcul = new Calcul(); 
+	 
+	@Test
+	public void testCalculerSom() {
+		Assert.assertEquals(25, calcul.calculerSom(10, 15));
+	} 
+
+	@Test
+	public void testCalculerDiff() {
+		Assert.assertEquals(30, calcul.calculerDiff(40, 10));
+	}
+
+}
